04c5190170acaee63d1bc84741a94e1a373ea7a5
Erdim Aydemir <erdimaydemir0@gmail.com>
2021-07-26 22:12:13 +0300

    çalışamalar-1

---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 1.5;
+            urun1.Aciklama = "starking";
+            urun1.StokAdedi = 15;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 5;
+            urun2.Aciklama = "Diyarbakır";
+            urun2.StokAdedi = 150;
+            Urun[] Urunler = new Urun[] { urun1,urun2 };
+
+            foreach (Urun urun in Urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.StokAdedi);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("-----------metotlar----------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            
+
+           
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 50;
+            product1.UnitsInStock = 5;
+
+            Product product2 = new Product { Id = 2,CategoryId=5, ProductName="Kitap", 
+                UnitPrice=20, UnitsInStock=150};
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
